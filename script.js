a894c9f6d0f93dfb910e46e78ca05179cdc910db
Rajat Pathania <83996978+pathania0001@users.noreply.github.com>
2025-07-23 12:17:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const tbody = document.getElementById("output");
+const promise = (time)=>{ 
+	return new Promise((resolve,reject)=>{
+	setTimeout(()=>{resolve(time/1000)},time)
+})};
+
+const exePromise = async()=>{
+	const startTime = performance.now();
+	const p1 = promise(2000);
+	const p2 = promise(1000);
+	const p3 = promise(3000);
+ Promise.all([p1,p2,p3]).then((data)=>{
+	 document.querySelector(".loading").style.display = "none";
+	 data.forEach((res,index)=>{
+			tbody.innerHTML += `
+        <tr>
+          <td>Promise ${index + 1}</td>
+          <td>${res}</td>
+        </tr>
+      `;
+	 });
+	 tbody.innerHTML += `<tr><td>Total</td><td>${((performance.now()-startTime)/1000).toFixed(3
+																							 )}</td></tr>`
+ })
+	
+}
+exePromise()
